90d05989461ec88ef4aead8efbcfda2c43251ce9
Rabee-Gh <rabeeghanem0@gmail.com>
2025-12-24 18:04:20 +0200

    x

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  output: 'standalone', 
+  distDir: '.next',
+  
+  images: {
+    unoptimized: true, 
+    
+    domains: ['your-domain.com'],
+  },
+}
 
 export default nextConfig;
